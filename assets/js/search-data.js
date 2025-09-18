5a61e8dde51cad33aefb84d43eb55c904c3bfb32
StefanStrain <stefan.strain@outlook.com>
2025-09-18 17:24:12 +0000

    Deploying to gh-pages from @ StefanStrain/stefanstrain.github.io@fee8960e213df88746d6067ba29c361785272f4e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,49 +9,35 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of my projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Welcome! This is a new space where I&#39;ll be sharing my projects. I&#39;m actively working on adding more of my work, so please check back soon for updates. Thanks for stopping by!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is a more detailed Curriculum Vitae representing my skills and experiences in more depth, for a quick read please consult the pdf version.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "nav-bookshelf",
+          title: "bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -414,11 +400,66 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-4-3-2-1",
+          title: '4 3 2 1',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
+              window.location.href = "/books/4_3_2_1/";
+            },},{id: "books-a-mind-for-numbers",
+          title: 'A Mind for Numbers',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/a_mind_for_numbers/";
+            },},{id: "books-the-goldfinch",
+          title: 'The Goldfinch',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/goldfinch/";
+            },},{id: "books-his-dark-materials",
+          title: 'His Dark Materials',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/his_dark_materials/";
+            },},{id: "books-the-80-20-principle",
+          title: 'The 80/20 Principle',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_80_20_principle/";
+            },},{id: "books-the-brain-that-changes-itself",
+          title: 'The Brain That Changes Itself',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_brain_that_changes_itself/";
+            },},{id: "books-the-hare-with-amber-eyes",
+          title: 'The Hare with Amber Eyes',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_hare_with_amber_eyes/";
+            },},{id: "books-the-overstory",
+          title: 'The Overstory',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_overstory/";
+            },},{id: "books-the-power-of-habit",
+          title: 'The Power of Habit',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_power_of_habit/";
+            },},{id: "books-thinking-fast-and-slow",
+          title: 'Thinking Fast and Slow',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/thinking_fast_and_slow/";
+            },},{id: "books-twenty-thousand-leagues-under-the-sea",
+          title: 'Twenty Thousand Leagues Under the Sea',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/twenty_thousand_leagues_under_the_sea/";
+            },},{id: "books-zero-to-one",
+          title: 'Zero to One',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/zero_to_one/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -430,51 +471,21 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-brain-tumor-classifier",
+          title: 'Brain Tumor Classifier',
+          description: "In development",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/mri_classifier/";
+            },},{id: "projects-ai-public-speaking-coach",
+          title: 'AI public speaking coach',
+          description: "in development",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/public_speaking/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -483,32 +494,25 @@ ninja.data = [{
           window.open("mailto:%73%74%65%66%61%6E.%73%74%72%61%69%6E@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/StefanStrain", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-kaggle',
+        title: 'Kaggle',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.kaggle.com/stefanstrainovic", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/stefanstrainovic", "_blank");
         },
       },{
       id: 'light-theme',
